bbbcae93dcf5acab28133fdb3a0460bc18f47a8f
raniyadev <raniyanaser77@gmail.com>
2026-01-26 17:20:57 +0400

    fix vite base path for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,15 @@ import "./index.css"
 import Navbar from "./components/ShopUi/Navbar"
 import ProductSection from "./components/ShopUi/ProductSection"
 
-
-
-
 function App() {
-
-
 
   return (
     <div>
       <Navbar />
       <ProductSection />
-
-     
-
     </div>
   )
 }
-
 
 export default App
 
